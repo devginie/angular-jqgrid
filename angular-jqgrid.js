0234381dd06888bf49b464e7190306efa7298a0d
devginie <devginie@users.noreply.github.com>
2016-05-27 14:08:41 +0900

    Add feature to keep TreeGrid structure when TreeGrid data updated.
    Add feature to keep TreeGrid structure when TreeGrid data updated.

--- angular-jqgrid.js
+++ angular-jqgrid.js
@@ -1,26 +1,26 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2016 Develer devginie
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+* The MIT License (MIT)
+*
+* Copyright (c) 2016 Develer devginie
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
 
 /* global $ */
 /* global angular */
@@ -61,8 +61,8 @@ angular.module('angular-jqgrid', []).directive('jqGrid', function() {
       };
 
       /**
-       * Watches
-       */
+      * Watches
+      */
       var onOptionsChanged = function() {
         jqGrid = init();
       };
@@ -71,6 +71,24 @@ angular.module('angular-jqgrid', []).directive('jqGrid', function() {
 
       var onDatasetChanged = function(dataset) {
         if (jqGrid) {
+
+          jqGrid.jqGrid('clearGridData');
+          // // Bugfix : 갱신 후 undefined 행을 업애기 위해 gridData 길이를 체크 후 빈값 할당
+          if( jqGrid.get(0).p.treeGrid) {
+            window.console.log('reloadTreeGrid : ', dataset);
+            jqGrid.get(0).addJSONData({
+              rows : dataset
+            });
+          }
+          else {
+            jqGrid.jqGrid('setGridParam', {
+              datatype: 'local',
+              data: dataset,
+              rowNum: dataset.length
+            });
+          }
+          //
+          // jqGrid.trigger('reloadGrid');
 
           return jqGrid.trigger('reload');
 
@@ -84,8 +102,8 @@ angular.module('angular-jqgrid', []).directive('jqGrid', function() {
       var unwatchDataset = scope.$watch('dataset', onDatasetChanged, true);
 
       /**
-       * Tear Down
-       */
+      * Tear Down
+      */
       element.on('$destroy', function onDestroy() {
         unwatchDataset();
         unwatchOptions();
